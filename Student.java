2bfc651e5f6ccccd8fbd3068b555494430338b07
Aabhash311 <156099878+Aabhash311@users.noreply.github.com>
2025-01-12 08:26:57 +0545

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
