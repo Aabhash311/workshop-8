7d80b12f5d1520f01f7c4d93125c589292492313
Aabhash311 <156099878+Aabhash311@users.noreply.github.com>
2025-01-12 08:27:54 +0545

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,5 @@
 public class Student {
+    // Attributes
     private String name;
     private int age;
 
@@ -6,5 +7,29 @@ public class Student {
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Method to display student information
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age: " + age);
     }
 }
